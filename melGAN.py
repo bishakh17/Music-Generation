28599bac3a8864d9cab9f05a57ea9ed3fda0992d
bishakh17 <telbdzone@gmail.com>
2023-04-05 14:40:14 +0530

    modified

--- melGAN.py
+++ melGAN.py
@@ -11,7 +11,7 @@ class Generator(nn.Module):
 
         self.activ = nn.SELU()
 
-        self.conv1 = nn.ConvTranspose2d(in_channels=50, out_channels=256, kernel_size=(3,2), stride=2, padding=0)
+        self.conv1 = nn.ConvTranspose2d(in_channels=20, out_channels=256, kernel_size=(3,2), stride=2, padding=0)
 
         self.conv2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=(3,2), stride=2, padding=0)
 
@@ -49,9 +49,9 @@ class Discriminator(nn.Module):
 
         self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,2), stride=2, padding=0)
 
-        # self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,2), stride=2, padding=0)
+        self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,2), stride=2, padding=0)
 
-        self.fc1 = nn.Linear(64*3*2, 64)
+        self.fc1 = nn.Linear(128, 64)
 
         self.fc2 = nn.Linear(64, 1)
  
@@ -61,11 +61,9 @@ class Discriminator(nn.Module):
         x = self.activ(self.conv2(x))
         x = self.activ(self.conv3(x))
         x = self.activ(self.conv4(x))
-        # x = self.activ(self.conv5(x))
+        x = self.activ(self.conv5(x))
         x = torch.flatten(x, 1)
-        x = self.activ(x)
-        x = self.fc1(x)
-        x = self.activ(x)
+        x = self.activ(self.fc1(x))
         x = self.fc2(x)
         x = self.sigmoid(x)
         return x
@@ -76,7 +74,7 @@ class Discriminator(nn.Module):
 
 if __name__ == "__main__":
     gen = Generator()
-    summary(gen, (50, 1, 1))
+    summary(gen, (20, 1, 1))
 
     disc = Discriminator()
     summary(disc, (1, 64, 16))
